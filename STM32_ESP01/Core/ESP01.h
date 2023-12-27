7fae7a84ad656d631bbf2c7080056f6c628fe19d
Greatreyhan <greatreyhan@students.undip.ac.id>
2023-12-28 00:30:20 +0700

    ready to use

---
+++ STM32_ESP01/Core/ESP01.h
@@ -0,0 +1,17 @@
+/*
+ * ESP01.h
+ *
+ *  Created on: Dec 27, 2020
+ *      Author: Greatreyhan
+ */
+
+#ifndef INC_ESP01_H_
+#define INC_ESP01_H_
+
+
+void ESP_Init (char *SSID, char *PASSWD);
+
+void Server_Start (void);
+
+
+#endif /* INC_ESP8266_HAL_H_ */
---
+++ STM32_ESP01/Debug/Core/subdir.mk
@@ -0,0 +1,30 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (11.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Core/ESP01.c \
+../Core/Ring_Buffer.c 
+
+OBJS += \
+./Core/ESP01.o \
+./Core/Ring_Buffer.o 
+
+C_DEPS += \
+./Core/ESP01.d \
+./Core/Ring_Buffer.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Core/%.o Core/%.su Core/%.cyclo: ../Core/%.c Core/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F401xC -c -I../Core/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
+clean: clean-Core
+
+clean-Core:
+	-$(RM) ./Core/ESP01.cyclo ./Core/ESP01.d ./Core/ESP01.o ./Core/ESP01.su ./Core/Ring_Buffer.cyclo ./Core/Ring_Buffer.d ./Core/Ring_Buffer.o ./Core/Ring_Buffer.su
+
+.PHONY: clean-Core
+
